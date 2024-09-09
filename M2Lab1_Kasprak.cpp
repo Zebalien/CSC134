c2cc351d5834df06068ae1c7b15359093587af63
Zebalien <kaspraki4970@student.faytechcc.edu>
2024-09-09 15:34:07 +0000

    clean Up

--- M2Lab1_Kasprak.cpp
+++ M2Lab1_Kasprak.cpp
@@ -10,10 +10,49 @@ int main()
 {
     // set up variables
     string name;
+    int length;
+    int width;
+    int height;
+    int area;
+    int volume;
+
+    // price variables in dollars
+    const double COST_PER_CUBIC_FT = .23;
+    const double PRICE_PER_CU_FT = .5;
+    double cost;
+    double customer_price;
+    double profit;
 
     // say hi
+    /*
     cout << "Hi, what's your name?";
     cin >> name;
     cout << "Nice to meet you, " << name << endl;
+    */
+
+    // ask user questions
+    cout << "Let's get the crate dimensions (in feet)." << endl;
+    cout << "What's the length? ";
+    cin >> length;
+    cout << "What's the width? ";
+    cin >> width;
+    cout << "What's the height? ";
+    cin >> height;
+    cout << "Length: " << length << ", Width: " << width << ", Height: " << height << endl;
+
+    // calculations
+    volume = length * width * height;
+    // cost clculations
+    cost = volume * COST_PER_CUBIC_FT;
+    customer_price = volume * PRICE_PER_CU_FT;
+    profit = customer_price - cost;
+
+    // output
+   cout << "Crate is: " << width << "x" << length << "x" << height << endl;
+   cout << "Total volume is: " << volume << " cubic feet." << endl;
+   cout << "The cost to make the crate is: $" << cost << endl;
+   cout << "The price of the crate is: $" << customer_price << endl;
+   cout << "The profit made is: $" << profit << endl;
+   
 
 }
