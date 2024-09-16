6d59319eea6ade1c71bbb5eaa7b149d669630d31
Zebalien <kaspraki4970@student.faytechcc.edu>
2024-09-16 14:33:20 +0000

    I did smth

---
+++ M3/M3T1.cpp
@@ -0,0 +1,14 @@
+//M3T1
+//CSC134
+//9-16-24
+//Isaac Kasprak
+
+//Ask the user to enter the length and width of two rectangles,
+//then display the area of both
+
+#include <iostream>
+using namespace std;
+int main()  {
+    //set up variable
+    cout << "M3T1" << endl;
+}
