511f4e013718e1270ad385043aaed0c4dc96b390
Zebalien <kaspraki4970@student.faytechcc.edu>
2024-09-16 14:47:15 +0000

    Finish M3T1

--- M3/M3T1.cpp
+++ M3/M3T1.cpp
@@ -15,8 +15,21 @@ int main()  {
 
     cout << "M3T1-Area of two rectagles" << endl;
     cout << "Width of first rectangle" << endl;
-    cin << width1;
-    cout << "length of first rectangle" << endl;
-    cin << length1;
+    cin >> width1;
+    cout << "Length of first rectangle" << endl;
+    cin >> length1;
+    cout << endl;
+    cout << "Width of second rectangle" << endl;
+    cin >> width2;
+    cout << "Length of second rectangle" << endl;
+    cin >> length2;
+
+    //calculations
+    area1=width1*length1;
+    area2=width2*length2;
+
+    //response
+    cout << "The area of the first rectangle is " << area1 << endl;
+    cout << "The area of the second rectangle is " << area2 << endl;
 
 }
