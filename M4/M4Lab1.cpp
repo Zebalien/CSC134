e7e26084ac8ee96c0a8a65eded22d221cb75e15a
Zebalien <kaspraki4970@student.faytechcc.edu>
2024-10-07 15:01:39 +0000

    Loops

--- M4/M4Lab1.cpp
+++ M4/M4Lab1.cpp
@@ -5,3 +5,30 @@
 
 #include <iostream>
 using namespace std;
+
+int main() {
+    cout << "M4LAB1" << endl;
+    string emoji="👾";
+    //cout << "👾" << endl;
+    // print sideways (row)
+    for (int i=0; i<9; i++){
+        cout << "👾" << " ";
+    }
+    cout << endl; // finish the row
+
+    //print down (column) 
+    for (int j=0; j<9;j++){
+        cout << emoji << endl;
+    }
+    cout << "Full version: " << endl << endl;
+    const int rows = 4;
+    const int cols = 6;
+    //put both together, to fill in the rectangle
+    for (int i=0;i<rows;i++){
+        for (int j=0;j<cols;j++){
+            cout << emoji << " ";
+        }
+        cout << endl;
+    }
+ //HelloWorld("print")   
+}
